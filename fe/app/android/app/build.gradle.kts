c32a0254be355e197ff8b8135004ac1293c9fda5
thoconomcarot <nguyennhuquynhct2005@gmail.com>
2026-09-20 09:00:33 +0700

    update

--- fe/app/android/app/build.gradle.kts
+++ fe/app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.app"
-    compileSdk = 37
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
